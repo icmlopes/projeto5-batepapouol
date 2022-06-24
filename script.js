872d52b3079096fb55a5a82b7e66a60d8a2d09e5
Isabella <icmlopes@gmail.com>
2022-06-24 08:51:32 -0300

    atualização

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const adMsg = [
+/* const adMsg = [
     {nome: "Qual seu nome?"}
-]
+] */
+let nickName;
+/* let horas = getHours(); */ /* vai informar o horário da msg de acordo com o horário local em 00:00:00 */
 
-function enviarMensagem(){
-    const novaMensagem = Document.querySelector("input").value;
 
-    const adMsg = {
-        nome: novaMensagem;
+function iniciarChat() {
+    while (nickName === undefined) {
+        nickName = String(prompt("Qual o seu nome?"));
     }
 }
+iniciarChat();
+
+
+/* function enviarMensagem(){
+    const novaMensagem = Document.querySelector("input").value;
+
+    const adNovaMsg = {
+        nome: nickName
+    }
+    adMsg.push(adNovaMsg);
+    console.loog(enviarMensagem);
+}
+ */
