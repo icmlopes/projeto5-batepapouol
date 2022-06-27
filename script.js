c8f2941809ba7339dc1c103d76caddb55e4eec1e
Isabella <icmlopes@gmail.com>
2022-06-27 18:07:52 -0300

    atualização

--- script.js
+++ script.js
@@ -1,25 +1,107 @@
-/* const adMsg = [
-    {nome: "Qual seu nome?"}
-] */
-let nickName;
+let user = { name: "" };
+const sentMessage = [
+    {
+        from: "",
+        to: "todos",
+        text: "",
+        type: "",
+        time: ""
+    }
+]
+let mensagem = [];
+let participantes = [];
+
+function iniciarChat() {
+    while (!user.name) {
+        user.name = prompt("Qual o seu nome?");
+    }
+    console.log(user.name);
+}
+
+askUserName();
+
+function askUserName() {
+    iniciarChat();
+    const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", user);
+    promessa.then(beginChat);
+    promessa.catch(userError);
+}
+
+function beginChat() {
+    loadMessage();
+    atualizarMensagem();
+    atualizarUsuarios();
+    
+    const descer = document.querySelector('.chat')
+    descer.scrollIntoView();
+}
+
+function userError() {
+    alert("Nickname já existente, tente outro...");
+    return iniciarChat();
+}
+
 /* let horas = getHours(); */ /* vai informar o horário da msg de acordo com o horário local em 00:00:00 */
 
 
-function iniciarChat() {
-    while (nickName === undefined) {
-        nickName = String(prompt("Qual o seu nome?"));
+/* Buscar usuários existentes */
+function buscarUsuario() {
+    const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+
+    promessa.then(processarParticipantes);
+    promessa.catch(reloadChat);
+}
+buscarUsuario();
+
+/* Carregar mensagens existentes */
+function loadMessage() {
+    const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promessa.then(processMessage)
+}
+
+
+function processMessage(answer) {
+    for (let i = 0; i < answer.data.lenght; i++) {
+        const message = answer.data[i];
+        answer.push;
     }
 }
-iniciarChat();
+processMessage();
 
-
-/* function enviarMensagem(){
-    const novaMensagem = Document.querySelector("input").value;
-
-    const adNovaMsg = {
-        nome: nickName
-    }
-    adMsg.push(adNovaMsg);
-    console.loog(enviarMensagem);
+function enviarMsg() {
+    const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", sentMessage);
+    promessa.then(loadMessage);
+    promessa.catch(reloadChat);
 }
- */
+
+/* function processarParticipantes(resposta) {
+    participantes = resposta.data;
+
+};
+processarParticipantes(); */
+
+function reloadChat() {
+    window.location.reload();
+    alert("Iih, caiu! =/");
+}
+
+function atualizarMensagem() {
+    setInterval(loadMessage, 3000)
+};
+function atualizarUsuarios() {
+    setInterval(buscarUsuario, 5000)
+};
+
+function renderizarMensagem(){
+    const todasMsg = document.querySelector(".chat");
+    todasMsg.innerHTML = "";
+
+    for(let i = 0; i < mensagem.length; i++){
+        todasMsg;innerHTML += `
+        <li class="mensagem">
+            <span><span class="horario">(${mensagem[i].time})</span></span>
+        </li>`
+}
+}
+renderizarMensagem();
+
